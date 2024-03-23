4c4d7f6300b16e4788461b70bf578105673fba9f
AkhmetovTimur <akhmetov.tr@mail.ru>
2024-03-23 11:24:22 +0400

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,10 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setColor("Белый");
+        car1.setModel("Лада Веста");
+        car1.setYear(2020);
+        System.out.println("Автомобиль " + car1.getModel() + " цвет " + car1.getColor() + " год выпуска " + car1.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,7 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike1 = new Motorbike("ИЖ Планета 5", "Красный", 1990);
+        System.out.println("Мотоцикл " + motorbike1.getModel() + " цвет " + motorbike1.getColor() + " год выпуска " + motorbike1.getYear());
     }
 }
