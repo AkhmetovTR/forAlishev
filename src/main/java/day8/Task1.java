cdbfe41f06f9af99bb10113569c1f9910f34e796
AkhmetovTimur <akhmetov.tr@mail.ru>
2024-03-26 13:19:29 +0400

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,27 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String stringSpace = " ";
+        String stringAll = "";
+        StringBuilder sb = new StringBuilder();
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            stringAll = stringAll + stringSpace + i;
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println(stringAll);
+
+        long startTimeSB = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i + " ");
+        }
+        long stopTimeSB = System.currentTimeMillis();
+        System.out.println(sb);
+        System.out.println("Использование строк");
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
+        System.out.println("Использование StringBuilder");
+        System.out.println("Длительность работы, в мс.: " + (stopTimeSB - startTimeSB));
 
     }
 }
