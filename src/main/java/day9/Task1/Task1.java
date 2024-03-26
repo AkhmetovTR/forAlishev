bf8f8bb76e5310bae05543ebd95adc9c3f81b7b0
AkhmetovTimur <akhmetov.tr@mail.ru>
2024-03-26 18:19:20 +0400

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,11 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Student student1 = new Student("Петя","Группа 1");
+        Teacher teacher1 = new Teacher("Татьяна","Математика");
+        System.out.println(student1.getGroupName());
+        System.out.println(teacher1.getSubjectName());
+        student1.printInfo();
+        teacher1.printInfo();
     }
 }
